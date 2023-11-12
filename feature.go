ad74ca08966d3f5d09159cecb21d80089ef59496
chenbiao <149027310@qq.com>
2023-11-12 15:59:23 +0800

    init

--- feature.go
+++ feature.go
@@ -5,21 +5,17 @@ import (
 	dgctx "github.com/darwinOrg/go-common/context"
 	dghttp "github.com/darwinOrg/go-httpclient"
 	"github.com/google/uuid"
+	"strings"
 	"time"
-)
-
-const (
-	registerFeaturePath = "/res/feature/v1/register"
-	updateFeaturePath   = "/res/feature/v1/update"
-	deleteFeaturePath   = "/res/feature/v1/delete"
 )
 
 type AudioType string
 
 const (
-	AudioTypeRaw     = "raw"
-	AudioTypeSpeex   = "speex"
-	AudioTypeOpusOgg = "opus-ogg"
+	AudioTypeRaw     AudioType = "raw"
+	AudioTypeSpeex   AudioType = "speex"
+	AudioTypeOpusOgg AudioType = "opus-ogg"
+	failStatus                 = 2
 )
 
 type RegisterFeatureRequest struct {
@@ -43,7 +39,73 @@ type UpdateFeatureResponse struct {
 	Status int `json:"status"`
 }
 
+type DeleteFeatureRequest struct {
+	FeatureIds []string `json:"feature_ids" binding:"required,minLength=1"`
+}
+
+type DeleteFeatureResponse struct {
+	DelFailIds string `json:"del_fail_ids"`
+}
+
 func (c *Client) RegisterFeature(ctx *dgctx.DgContext, req *RegisterFeatureRequest) (string, error) {
+	params, header := c.buildParamsAndHeader(ctx)
+	url := c.Config.Host + "/res/feature/v1/register?" + formUrlEncodedParams(params)
+	rt, err := dghttp.DoPostJsonToStruct[IflytekResult[RegisterFeatureResponse]](ctx, url, req, header)
+	if err != nil {
+		return "", err
+	}
+
+	if !rt.isSuccess() {
+		return "", errors.New(rt.Desc)
+	}
+
+	if rt.Data.Status == failStatus || rt.Data.FeatureId == "" {
+		return "", errors.New("注册失败")
+	}
+
+	return rt.Data.FeatureId, nil
+}
+
+func (c *Client) UpdateFeature(ctx *dgctx.DgContext, req *UpdateFeatureRequest) error {
+	params, header := c.buildParamsAndHeader(ctx)
+	url := c.Config.Host + "/res/feature/v1/update?" + formUrlEncodedParams(params)
+	rt, err := dghttp.DoPostJsonToStruct[IflytekResult[UpdateFeatureResponse]](ctx, url, req, header)
+	if err != nil {
+		return err
+	}
+
+	if !rt.isSuccess() {
+		return errors.New(rt.Desc)
+	}
+
+	if rt.Data.Status == failStatus {
+		return errors.New("更新失败")
+	}
+
+	return nil
+}
+
+func (c *Client) DeleteFeature(ctx *dgctx.DgContext, featureIds []string) []string {
+	req := map[string]any{"feature_ids": featureIds}
+	params, header := c.buildParamsAndHeader(ctx)
+	url := c.Config.Host + "/res/feature/v1/update?" + formUrlEncodedParams(params)
+	rt, err := dghttp.DoPostJsonToStruct[IflytekResult[DeleteFeatureResponse]](ctx, url, req, header)
+	if err != nil {
+		return featureIds
+	}
+
+	if !rt.isSuccess() {
+		return featureIds
+	}
+
+	if rt.Data.DelFailIds == "" {
+		return []string{}
+	}
+
+	return strings.Split(rt.Data.DelFailIds, ";")
+}
+
+func (c *Client) buildParamsAndHeader(ctx *dgctx.DgContext) (map[string]any, map[string]string) {
 	params := map[string]any{
 		"accessKeyId":     c.Config.AccessKeyId,
 		"dateTime":        time.Now().Format(timeFormat),
@@ -55,27 +117,5 @@ func (c *Client) RegisterFeature(ctx *dgctx.DgContext, req *RegisterFeatureReque
 		"x-traceid": ctx.TraceId,
 	}
 
-	url := c.Config.Host + "?" + formUrlEncodedParams(params)
-	rt, err := dghttp.DoPostJsonToStruct[IflytekResult[RegisterFeatureResponse]](ctx, url, req, header)
-	if err != nil {
-		return "", err
-	}
-
-	if !rt.isSuccess() {
-		return "", errors.New(rt.Desc)
-	}
-
-	if rt.Data.Status == 2 || rt.Data.FeatureId == "" {
-		return "", errors.New("注册失败")
-	}
-
-	return rt.Data.FeatureId, nil
-}
-
-func (c *Client) UpdateFeature(req *UpdateFeatureRequest) error {
-	return nil
-}
-
-func (c *Client) DeleteFeature(featureIds []string) []string {
-	return nil
+	return params, header
 }
